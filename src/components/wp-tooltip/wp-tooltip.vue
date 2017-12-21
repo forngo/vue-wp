a6f2d82ceabf620b68591ccf9bdf8daceef5da79
Alessandro Tesoro <mythemesdepot@gmail.com>
2017-12-21 12:40:06 +0000

    add tooltip component

--- src/components/wp-tooltip/wp-tooltip.vue
+++ src/components/wp-tooltip/wp-tooltip.vue
@@ -1,11 +1,54 @@
 <template>
-
+    <span :data-label="label" :class="classes" >
+        <slot/>
+    </span>
 </template>
 
 <script>
+import { oneOf } from '../../utils/assist';
+
 export default {
     name: 'wp-tooltip',
     props: {
-    }
+        active: {
+            type: Boolean,
+            default: true
+        },
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        label: String,
+        position: {
+            default: 'top',
+            validator (value) {
+                return oneOf(value, ['top', 'bottom', 'left', 'right']);
+            }
+        },
+        always: Boolean,
+        square: Boolean,
+        multilined: Boolean,
+        size: {
+            type: String,
+            default: 'medium',
+            validator (value) {
+                return oneOf(value, ['small', 'medium', 'large']);
+            }
+        }
+    },
+    computed: {
+        classes () {
+            return [
+                'wp-tooltip',
+                'is-' + this.type,
+                'is-' + this.position,
+                'is-' + this.size,
+                this.active ? 'tooltip' : '',
+                this.square ? 'is-square' : '',
+                this.always ? 'is-always' : '',
+                this.multilined ? 'is-multiline' : '',
+            ];
+        }
+    },
 }
 </script>
