4fdac2a6f898242858b3a394631e6109be5c2321
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-01-18 13:59:39 +0000

    change style of large modal

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ import WPTooltip from './components/wp-tooltip'
 import Shepherd from 'tether-shepherd'
 import config, { setOptions } from './utils/config'
 
+import './scss/main-build.scss'
+
 const components = {
     WPAccordionItem,
     WPAccordions,
